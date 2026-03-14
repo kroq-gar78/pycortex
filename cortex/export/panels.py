6da828d94a810bad8fa7c9ad171a2163b85381e8
Aditya Vaidya <kroq-gar78@users.noreply.github.com>
2026-03-13 18:47:04 -0700

    webshow should allow any Dataview, not just Vertex/Volume

--- cortex/export/panels.py
+++ cortex/export/panels.py
@@ -15,7 +15,7 @@ import numpy.typing as npt
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 
-from ..dataset import Volume, Vertex
+from ..dataset import Dataview
 from .save_views import save_3d_views, ViewParams
 from ._default_params import (
     params_inflatedless_lateral_medial_ventral,
@@ -35,7 +35,7 @@ class PanelParams(TypedDict):
     extent: tuple[float, float, float, float]
 
 def plot_panels(
-    volume: Union[Volume, Vertex],
+    volume: Dataview,
     panels: list[PanelParams],
     figsize: tuple[float, float]=(16, 9),
     windowsize: tuple[int, int]=(1600 * 4, 900 * 4),
